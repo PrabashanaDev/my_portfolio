002c63d36ab1122f3c529fd23f80bcaaf6240394
Chathura Prabashana <prabashana.dev@gmail.com>
2023-12-28 11:35:37 +0000

    Name and socials update

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Chathura Prabashana",
+  title: "DevOps Engineer",
+  email: "prabashana.dev@gmail.com",
+  gitHub: "PrabashanaDev",
+  instagram: "Chathura Prabashana",
+  linkedIn: "Chathura Prabashana",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Chathura Prabashana",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
